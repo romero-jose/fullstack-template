cf6d038ed29ae15b7d5ec877221dce6fff75d662
José Luis Romero <jose.romero@ing.uchile.cl>
2026-03-11 15:29:25 -0300

    Adds cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/api/health', async (_req, res) => {
